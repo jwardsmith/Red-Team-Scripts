5908ab2f8a1145d031c24e11ca1548327bdcef9d
jwardsmith <31498830+jwardsmith@users.noreply.github.com>
2021-06-07 08:11:49 +1000

    Create user-properties.ps1

---
+++ Active Directory/user-properties.ps1
@@ -0,0 +1,30 @@
+$domainObj = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
+
+$PDC = ($domainObj.PdcRoleOwner).Name
+
+$SearchString = "LDAP://"
+
+$SearchString += $PDC + "/"
+
+$DistinguishedName = "DC=$($domainObj.Name.Replace('.', ',DC='))"
+
+$SearchString += $DistinguishedName
+
+$Searcher= New-Object System.DirectoryServices.DirectorySearcher([ADSI]$SearchString)
+
+$objDomain = New-Object System.DirectoryServices.DirectoryEntry
+
+$Searcher.SearchRoot = $objDomain
+
+$Searcher.filter="samAccountType=805306368"
+
+$Result = $Searcher.FindAll()
+
+Foreach($obj in $Result)
+{
+	Foreach($prop in $obj.Properties)
+	{
+		$prop
+	}
+	Write-Host "------------------------"
+}
