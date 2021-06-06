339c8f42bbca6b0a24a201092ab3f0c025e923ff
jwardsmith <31498830+jwardsmith@users.noreply.github.com>
2021-06-07 08:11:30 +1000

    Create user-enum.ps1

---
+++ Active Directory/user-enum.ps1
@@ -0,0 +1,21 @@
+$domainObj = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
+
+$PDC = ($domainObj.PdcRoleOwner).Name
+
+$SearchString = "LDAP://"
+
+$SearchString += $PDC + "/"
+
+$DistinguishedName = "DC=$($domainObj.Name.Replace('.', ',DC='))"
+
+$SearchString += $DistinguishedName
+
+$Searcher = New-Object System.DirectoryServices.DirectorySearcher([ADSI]$SearchString)
+
+$objDomain = New-Object System.DirectoryServices.DirectoryEntry
+
+$Searcher.SearchRoot = $objDomain
+
+$Searcher.filter="samAccountType=805306368"
+
+$Searcher.FindAll()
